98f5474a1ca9576b48351858c018d34f072a30f7
Kalpakbaev <kozha_1992@mail.ru>
2022-12-14 01:14:51 +0500

    example03

---
+++ example03/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Напишите программу, которая задаёт массив из произвольного кол-ва элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+int[] Array(){
+Random myRandom = new Random();
+int [] array = new int[9];
+for(int i = 0; i < array.Length; i++){
+    array[i] = myRandom.Next(0 , 20 );
+}
+return array;
+}
+void ShowArray(int[] array){
+ for(int i = 0 ; i < array.Length; i++){
+    Console.Write($"{array[i]} ");
+ }
+}
+ShowArray(Array());
